0b698ad15780724691947bb9e8e19c0ef4294f3d
NathanMEvans <nathan.evans.ncl@gmail.com>
2023-03-04 12:25:19 +0000

    added exits struct

--- main.c
+++ main.c
@@ -8,36 +8,40 @@ struct Activity {
    char* description;
 };
 
+struct Exit {
+   char* title;
+   char* description;
+   struct Room* room;
+};
+
 struct Room {
-   char* intro;
    int n_exits;
    int n_activities;
-   char* exits[10];
+   struct Exit* exits[10];
    struct Activity* activities[10];
 };
 
-char* showRoom(struct Room room) {
+struct Exit* showRoom(struct Exit* exit) {
+   struct Room* room = exit->room;
+   char* intro = exit->description;
    clear();
    char* input = malloc(sizeof(char)*10);
    int inputI = 0;
    while (1) {
       if (inputI > 0) {
-         if (inputI <= room.n_activities) {
-            printf("%s\n", room.activities[inputI-1]->description);
+         if (inputI <= room->n_activities) {
+            printf("%s\n", room->activities[inputI-1]->description);
          } else {
-            return room.exits[inputI-1-room.n_activities];
+            return room->exits[inputI-1-room->n_activities];
          } 
       } else {
-          printf("%s\n",room.intro);
+          printf("%s\n",intro);
       }
-      for (int i = 0; i < room.n_activities; i++) {
-         printf("%d: %s\n",i+1,room.activities[i]->title);
+      for (int i = 0; i < room->n_activities; i++) {
+         printf("%d: %s\n",i+1,room->activities[i]->title);
       }
-      for (int i = 0; i < room.n_exits; i++) {
-         if (inputI == i+room.n_activities+1)  {
-            return room.exits[i];
-         }
-         printf("%d: go to %s\n",i+room.n_activities+1,room.exits[i]);
+      for (int i = 0; i < room->n_exits; i++) {
+         printf("%d: go to %s\n",i+room->n_activities+1,room->exits[i]->title);
       }
       
       fgets(input, 10, stdin);
@@ -53,22 +57,26 @@ int main() {
    struct Activity dogBed = {"dog bed", "You sleep for an hour"};
    struct Activity bigBed = {"big bed", "You sleep for an hour"};
    struct Room bedroom = {
-      "You wake up in the bedroom",
       1,
       2,
-      {"hallway"},
+      {},
       {&dogBed, &bigBed}
    };
    struct Activity frontDoor = {"front door", "You bark at the front door"};
    struct Room hallway = {
-      "You walk into the hallway",
-      2,
       1,
-      {"bedroom", "living room"},
+      1,
+      {},
       &frontDoor
    };
-   showRoom(bedroom);
-   showRoom(hallway);
+   struct Exit bedroomHallway = {"hallway", "You walk into the hallway", &hallway};
+   bedroom.exits[0] = &bedroomHallway;
+   struct Exit hallwayBedroom = {"bedroom", "You walk into the bedroom", &bedroom};
+   hallway.exits[0] = &hallwayBedroom;
+   struct Exit nextExit = {"bedroom", "You wake up in a big bed", &bedroom};
+   while (1) {
+      nextExit = *showRoom(&nextExit);
+   }
 }
 
 
